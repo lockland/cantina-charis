cb9a891f2f0843d7cb5cc0ee6e0714a01085c792
Sidney Souza <sidneysase@gmail.com>
2023-08-15 14:21:20 -0300

    add open cash machine form

--- client/src/components/pages/Home.tsx
+++ client/src/components/pages/Home.tsx
@@ -1,6 +1,63 @@
-function Home() {
+import { TextInput, Button, Group, Box, NumberInput } from '@mantine/core';
+import { useForm } from '@mantine/form';
+import "./Home.css"
+
+function InitialPage() {
+  const form = useForm({
+    initialValues: {
+      eventName: '',
+      initialCashValue: 0,
+      termsOfService: false,
+    },
+
+    // validate: {
+    //   email: (value) => (/^\S+@\S+$/.test(value) ? null : 'Invalid email'),
+    // },
+  });
+
   return (
-    <div >Caixa</div>
+
+    <div id="initial-page">
+      <Box maw={300} mx="auto">
+        <form onSubmit={form.onSubmit((values) => console.log(values))}>
+          <TextInput
+            withAsterisk
+            label="Evento"
+            placeholder="Nome do evento"
+            className='form-item'
+            {...form.getInputProps('eventName')}
+          />
+
+          <NumberInput
+            withAsterisk
+            label="Caixa"
+            placeholder="R$ 10000,00"
+            {...form.getInputProps('initialCashValue')}
+          />
+
+          <Group position="center">
+            <Button type="submit" fullWidth> Abrir Caixa </Button>
+          </Group>
+        </form>
+      </Box>
+    </div>
+
+  )
+}
+
+
+function CashPage() {
+  return <> Cash Page</>
+}
+
+
+const alreadCreated = false
+
+
+function Home() {
+
+  return (
+    (alreadCreated) ? <CashPage /> : <InitialPage />
   );
 }
 
