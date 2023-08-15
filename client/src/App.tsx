72f12672a888f79e7d63942c11f089f266a02519
Sidney Souza <sidneysase@gmail.com>
2023-08-14 22:08:28 -0300

    add home page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,12 +5,14 @@ import Products from "./components/pages/Products";
 import Orders from './components/pages/Orders';
 import Reports from './components/pages/Reports';
 import Debits from './components/pages/Debits';
+import Home from './components/pages/Home';
 
 function App() {
   return (
     <BrowserRouter>
       <NavBar />
       <Routes>
+        <Route path="/" element={<Home />} />
         <Route path="/products" element={<Products />} />
         <Route path="/orders" element={<Orders />} />
         <Route path="/reports" element={<Reports />} />
