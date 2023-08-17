ab0722bf0f3e8a127cb2c67213c0ebe137b47f35
Sidney Souza <sidneysase@gmail.com>
2023-08-17 11:18:45 -0300

    remove useless code

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -6,28 +6,21 @@ import Orders from './components/pages/Orders';
 import Reports from './components/pages/Reports';
 import Debits from './components/pages/Debits';
 import Home from './components/pages/Home';
-import { MantineProvider } from '@mantine/core';
 
 function App() {
   return (
     <div className='App' >
-      <MantineProvider withNormalizeCSS>
-        <BrowserRouter>
-          <NavBar />
-          <div className='pages'>
-            <div className='page'>
-              <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/products" element={<Products />} />
-                <Route path="/orders" element={<Orders />} />
-                <Route path="/reports" element={<Reports />} />
-                <Route path="/customers-debits" element={<Debits />} />
-              </Routes>
-            </div>
-          </div>
-        </BrowserRouter>
-      </MantineProvider>
-    </div>
+      <BrowserRouter>
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/products" element={<Products />} />
+          <Route path="/orders" element={<Orders />} />
+          <Route path="/reports" element={<Reports />} />
+          <Route path="/customers-debits" element={<Debits />} />
+        </Routes>
+      </BrowserRouter>
+    </div >
   )
 }
 export default App
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,9 +2,29 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { ButtonStylesParams, MantineProvider } from '@mantine/core'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <MantineProvider withNormalizeCSS
+      theme={{
+        components: {
+          Button: {
+            styles: (theme, params: ButtonStylesParams, { variant }) => ({
+              root: {
+                height: '2.625rem',
+                padding: '0 1.875rem',
+                backgroundColor:
+                  variant === 'filled'
+                    ? "var(--button-color)"
+                    : undefined,
+              },
+            }),
+          }
+        }
+      }}
+    >
+      <App />
+    </MantineProvider>
   </React.StrictMode>,
 )
