564328d72de5dd72a872a00c0558e330cf7e1188
Sidney Souza <sidneysase@gmail.com>
2023-08-17 11:04:58 -0300

    add css variables

--- client/src/components/pages/InitialPage.tsx
+++ client/src/components/pages/InitialPage.tsx
@@ -1,7 +1,6 @@
 
 import { TextInput, Button, Group, Box, NumberInput } from '@mantine/core';
 import { useForm } from '@mantine/form';
-import "./InitialPage.css"
 
 
 function InitialPage() {
@@ -9,41 +8,32 @@ function InitialPage() {
     initialValues: {
       eventName: '',
       initialCashValue: 0,
-    },
-
-    // validate: {
-    //   email: (value) => (/^\S+@\S+$/.test(value) ? null : 'Invalid email'),
-    // },
+    }
   });
 
   return (
+    <Box maw={300} mx="auto">
+      <form onSubmit={form.onSubmit((values) => console.log(values))}>
+        <TextInput
+          withAsterisk
+          label="Evento"
+          placeholder="Nome do evento"
+          className='form-item'
+          {...form.getInputProps('eventName')}
+        />
 
-    <div id="initial-page" sx={{
-      "display": "flex", "align-items": "center"
-    }}>
-      <Box maw={300} mx="auto">
-        <form onSubmit={form.onSubmit((values) => console.log(values))}>
-          <TextInput
-            withAsterisk
-            label="Evento"
-            placeholder="Nome do evento"
-            className='form-item'
-            {...form.getInputProps('eventName')}
-          />
+        <NumberInput
+          withAsterisk
+          label="Caixa"
+          placeholder="R$ 10000,00"
+          {...form.getInputProps('initialCashValue')}
+        />
 
-          <NumberInput
-            withAsterisk
-            label="Caixa"
-            placeholder="R$ 10000,00"
-            {...form.getInputProps('initialCashValue')}
-          />
-
-          <Group position="center">
-            <Button type="submit" fullWidth color='#000'> Abrir Caixa </Button>
-          </Group>
-        </form>
-      </Box>
-    </div >
+        <Group position="center">
+          <Button type="submit" fullWidth color='#000'> Abrir Caixa </Button>
+        </Group>
+      </form>
+    </Box>
 
   )
 }
