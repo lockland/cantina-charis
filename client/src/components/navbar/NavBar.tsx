08ce9cffd7aa1a3535a232c13fa5bade6c780836
Sidney Souza <sidneysase@gmail.com>
2023-08-23 16:10:37 -0300

    add responsivity to menu

--- client/src/components/navbar/NavBar.tsx
+++ client/src/components/navbar/NavBar.tsx
@@ -2,36 +2,41 @@ import { NavLink } from "react-router-dom";
 import "./NavBar.css"
 import { CloseBtn } from "./CloseBtn";
 import { Container } from "@mantine/core";
+import { useState } from "react";
 
 interface NavBarProps {
   maw: string
 }
 
 function NavBar({ maw }: NavBarProps) {
-  return (
-    <Container maw={maw} className="navbar-container">
-      <nav>
-        <ul>
-          <li>
-            <NavLink to="/"  >Caixa</NavLink>
-          </li>
-          <li>
-            <NavLink to="/orders">Pedidos</NavLink>
-          </li>
-          <li>
-            <NavLink to="/products">Produtos</NavLink>
-          </li>
-          <li>
-            <NavLink to="/customers-debits">Em Aberto</NavLink>
-          </li>
-          <li>
-            <NavLink to="/reports">Relatórios</NavLink>
-          </li>
-        </ul>
-      </nav>
+  const [open, setOpen] = useState(false)
 
-      <CloseBtn />
-    </Container>
+  return (
+    <div className="navbar">
+      <button onClick={() => setOpen(!open)}>Menu</button>
+      <Container maw={maw} className={"navbar-container " + (open ? "opened" : "closed")}>
+        <nav >
+          <ul>
+            <li>
+              <NavLink to="/"  >Caixa</NavLink>
+            </li>
+            <li>
+              <NavLink to="/orders">Pedidos</NavLink>
+            </li>
+            <li>
+              <NavLink to="/products">Produtos</NavLink>
+            </li>
+            <li>
+              <NavLink to="/customers-debits">Em Aberto</NavLink>
+            </li>
+            <li>
+              <NavLink to="/reports">Relatórios</NavLink>
+            </li>
+          </ul>
+        </nav>
+        <CloseBtn />
+      </Container>
+    </div>
 
   );
 }
