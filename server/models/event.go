0ec294e3eb525ba80f2e82e5aca89a7b0886bfe7
Sidney Souza <sidneysase@gmail.com>
2023-08-28 11:25:54 -0300

    fix: set an event default open amount value

--- server/models/event.go
+++ server/models/event.go
@@ -9,7 +9,7 @@ import (
 type Event struct {
 	ID         int             `json:"event_id"`
 	Name       string          `json:"event_name"`
-	OpenAmount decimal.Decimal `json:"open_amount"`
+	OpenAmount decimal.Decimal `json:"open_amount" gorm:"default:0.00"`
 	Open       bool            `json:"is_open" gorm:"default:true"`
 	CreatedAt  time.Time       `json:"created_at"`
 	UpdatedAt  time.Time       `json:"updated_at"`
