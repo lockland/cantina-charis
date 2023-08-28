550c11e6e2dd7450f808a7d80c0a304f4904d770
Sidney Souza <sidneysase@gmail.com>
2023-08-28 11:05:29 -0300

    feat: define outoing structure

--- server/database/sqlite.go
+++ server/database/sqlite.go
@@ -2,7 +2,6 @@ package database
 
 import (
 	"github.com/lockland/cantina-charis/server/models"
-
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
 	"gorm.io/gorm/logger"
@@ -13,11 +12,15 @@ var err error
 
 func Connect(url string) {
 	Conn, err = gorm.Open(sqlite.Open(url), &gorm.Config{
-		Logger: logger.Default.LogMode(logger.Info),
+		Logger:               logger.Default.LogMode(logger.Info),
+		FullSaveAssociations: true,
 	})
 	if err != nil {
 		panic("failed to connect database")
 	}
 
-	Conn.AutoMigrate(&models.Event{})
+	Conn.AutoMigrate(
+		new(models.Event),
+		new(models.Outgoing),
+	)
 }
--- server/models/outgoing.go
+++ server/models/outgoing.go
@@ -1,3 +1,17 @@
 package models
 
-type Outgoing struct{}
+import (
+	"time"
+
+	"github.com/shopspring/decimal"
+)
+
+type Outgoing struct {
+	ID          int             `json:"outgoing_id"`
+	Description string          `json:"outgoing_description"`
+	Amount      decimal.Decimal `json:"outgoing_amount" gorm:"default:0.00"`
+	CreatedAt   time.Time       `json:"created_at"`
+	UpdatedAt   time.Time       `json:"updated_at"`
+	EventID     int             `json:"event_id" validate:"required"`
+	Event       Event
+}
