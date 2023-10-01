112771b459f7eb1df14bcd4bfc257a318270a9d2
Sidney Souza <sidneysase@gmail.com>
2023-10-01 11:53:12 -0300

    feat: change product enable default status to true

--- server/models/product.go
+++ server/models/product.go
@@ -6,6 +6,6 @@ type Product struct {
 	ID           int             `json:"product_id"`
 	Name         string          `json:"product_name" gorm:"uniqueIndex"`
 	Price        decimal.Decimal `json:"product_price" gorm:"default:0.00"`
-	Enabled      bool            `json:"enabled" gorm:"default:false"`
+	Enabled      bool            `json:"enabled" gorm:"default:true"`
 	OrderProduct OrderProduct    `json:"order_details"`
 }
